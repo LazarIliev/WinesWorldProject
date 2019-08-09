7787fa8a680b18ae611e04d41acbc30f5cfec917
Lazar <lazarilievplovdiv@mail.bg>
2019-08-09 13:14:20 +0300

    xUnit testing set up

---
+++ Tests/WinesWorld.Tests/Common/WinesWorldDbContextInMemoryFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WinesWorld.Data;
+
+namespace WinesWorld.Tests.Common
+{
+    public static class WinesWorldDbContextInMemoryFactory
+    {
+
+        public static WinesWorldDbContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<WinesWorldDbContext>()
+               .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+               .Options;
+
+            return new WinesWorldDbContext(options);
+        }
+    }
+}
---
+++ Tests/WinesWorld.Tests/Service/WinesServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WinesWorld.Data;
+using WinesWorld.Data.Models;
+using WinesWorld.Tests.Common;
+using Xunit;
+
+namespace WinesWorld.Tests.Service
+{
+    public class WinesServiceTests
+    {
+        private List<Wine> GetDummyData()
+        {
+            return new List<Wine>
+            {
+                new Wine
+                {
+                    Colour = "white",
+                    Country = "Bulgaria",
+                    Description = "wine description describing",
+                    
+                },
+            };
+        }
+
+        private async Task SeedData(WinesWorldDbContext context)
+        {
+            context.AddRange(GetDummyData());
+            await context.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async Task GetAllWines_WithDummyData_ShouldReturnCorrectResult()
+        {
+            var context = WinesWorldDbContextInMemoryFactory.InitializeContext();
+
+            await SeedData(context);
+        }
+    }
+}
